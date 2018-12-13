cf1dc09190593db289e03aede9b8c63e23879e4a
ll9 <llwierer@web.de>
2018-12-13 20:52:36 +0100

    decend cache

---
+++ app/components/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+            <Button class="message" :text="msg" @tap="nav" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+
+import SecondPage from './SecondPage';
+
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    },
+    methods: {
+        nav() {
+            this.$navigateTo(SecondPage);
+        }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ app/components/SecondPage.vue
@@ -0,0 +1,64 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+            <WebView :src="src" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!',
+        src: `<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+    <style>
+      .map {
+        height: 400px;
+        width: 100%;
+      }
+    </style>
+    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"><\/script>
+    <title>OpenLayers example</title>
+  </head>
+  <body>
+    <h2>My Map</h2>
+    <div id="map" class="map"></div>
+    <script type="text/javascript">
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([12, 48.82]),
+          zoom: 6
+        })
+      });
+    <\/script>
+  </body>
+</html>`
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
